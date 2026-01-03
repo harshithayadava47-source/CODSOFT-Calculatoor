39101326032964506482d7292e3c14b154acd0c2
harshithayadava47-source <harshithayadava47@gmail.com>
2026-01-03 20:04:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+let display = document.getElementBYID("display");
+function appendValue(value) {
+  dispaly.value += value;
+}
+ function clearDisplay() {
+   display.value = "" ;
+ }
+   function deleteLast() {
+     dispaly.value = diaplay.value.slice(0, -1);
+   }
+    function calculate() {
+      try {
+        display.value= eval(dispaly.value);
+      } catch {
+        display.value = "Error";
+      }
+    }
